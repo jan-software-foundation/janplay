3055d0295028a6583a57cee6afb9c7b63a7414d1
janderedev <me@janderedev.xyz>
2021-07-19 21:32:28 +0200

    code

--- src/func/play_stream.rs
+++ src/func/play_stream.rs
@@ -1,18 +1,13 @@
-use rodio::{Decoder, OutputStream};
-use std::io::{BufReader, Read};
-use std::fs::File;
+use rodio::{Decoder, OutputStream, Source};
+use std::io::BufReader;
+use std::thread;
 
-pub async fn play(stream: std::process::ChildStdout) {
-    let path = ulid::Ulid::new().to_string();
-    let mut buf_reader = BufReader::new(stream);
-    
-    if !std::path::Path::new(&path).exists() {
-        std::fs::write(&path, "").unwrap();
-    }
-    
-    
-    
+pub async fn play(file: std::fs::File) {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let file = BufReader::new(File::open(&path).unwrap());
+    let file = BufReader::new(file);
     let source = Decoder::new(file).unwrap();
+    let  audio_length = source.total_duration().expect("Audio has unknown length");
+    stream_handle.play_raw(source.convert_samples()).unwrap();
+    
+    thread::sleep(audio_length);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,20 @@ extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 
+use std::fs;
+use std::path::Path;
+
 mod func;
 
 #[tokio::main]
 async fn main() {
+    // Create temp directory
+    let temp_dir = format!("{}/jancloud", std::env::temp_dir().to_str().unwrap());
+    if !Path::new(&temp_dir).is_dir() {
+        fs::create_dir(&temp_dir).expect("Failed to create temp dir");
+    }
+    
+    let mut gen = ulid::Generator::new();
     
     let url = func::fetch_audio_url::fetch_audio_url(String::from("https://www.youtube.com/watch?v=vvDogoFw4B8")).await.unwrap();
     let resp = reqwest::get(&url)
@@ -14,6 +24,17 @@ async fn main() {
     
     println!("{}", &url);
     
-    let stream = func::transcode_audio::transcode(resp).await;
-    func::play_stream::play(stream).await;
+    let filename = gen
+        .generate()
+        .expect("Failed to generate ulid")
+        .to_string();
+    
+    func::transcode_audio::transcode(resp, format!("{}/{}",temp_dir , &filename)).await;
+    
+    let file = fs::File::open(format!("{}/{}",temp_dir , &filename))
+        .expect("Failed to open file");
+    
+    func::play_stream::play(file).await;
+    fs::remove_file(format!("{}/{}",temp_dir , &filename))
+        .expect("Failed to delete temporary file");
 }
